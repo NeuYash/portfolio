53cd20e70febd7ca68fb552b0f5d9900296d0fc0
NeuYash <pawar.ya@northeastern.edu>
2023-06-13 16:19:16 -0400

    scrollspy removed for exp and contact

--- assets/js/script.js
+++ assets/js/script.js
@@ -291,9 +291,9 @@ srtop.reveal('.education .box', { interval: 200 });
 srtop.reveal('.work .box', { interval: 200 });
 
 /* SCROLL EXPERIENCE */
-srtop.reveal('.experience .timeline', { delay: 400 });
-srtop.reveal('.experience .timeline .container', { interval: 400 });
+// srtop.reveal('.experience .timeline', { delay: 400 });
+// srtop.reveal('.experience .timeline .container', { interval: 400 });
 
 /* SCROLL CONTACT */
-srtop.reveal('.contact .container', { delay: 400 });
-srtop.reveal('.contact .container .form-group', { delay: 400 });
+// srtop.reveal('.contact .container', { delay: 400 });
+// srtop.reveal('.contact .container .form-group', { delay: 400 });
