eb4d9512c6b4c9a191f07e1fc3250eca10ea2a81
NeuYash <pawar.ya@northeastern.edu>
2024-02-12 16:16:18 -0500

    Subtle updates: Content, New Position, Resume, Youtube logo and links

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Competitive Programming", "Frontend development", "Backend development", "FullStack development", "Open-Source Contributor"],
+    strings: ["Competitive Programming", "Frontend development", "Backend development", "FullStack development"],
     loop: true,
     typeSpeed: 25,
     backSpeed: 25,
